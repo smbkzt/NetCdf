3fa54020b41aef40ffad4fb861f3c5e75bfe77c6
Bekzat Shayakhmetov <bekzat.95@mail.ru>
2016-12-28 17:10:25 +0700

    sout changed

--- src/com/eazydev/HtmlParser.java
+++ src/com/eazydev/HtmlParser.java
@@ -9,12 +9,15 @@ import java.io.IOException;
 public class HtmlParser {
     public void parser(String url) {
         String urlToParse;
+        int count = 0;
 
+        // Проверяем ссылку
         if (!url.startsWith("http")){
             urlToParse = "http://" + url;
         }
         else urlToParse = url;
 
+        // Получаем страницу
         Document doc = null;
         try {
             doc = Jsoup.connect("" + urlToParse).get();
@@ -26,19 +29,24 @@ public class HtmlParser {
         System.out.println("___________________________");
         System.out.println(title);
         System.out.println("___________________________");
-        System.out.println("File Name                         |       Link");
+        System.out.println("Files: ");
 
+        // Получаем все элементы из документа по тэгу
         Elements links = doc.getElementsByTag("a");
         for (Element link : links) {
             String linkHref = link.attr("href");
             String linkText = link.text();
 
             if (linkHref.endsWith(".nc")){
-                if (linkHref.startsWith("http"))
+                count++;
+                System.out.print(count + ") ");
+
+                if (linkHref.startsWith("http")) {
                     System.out.println(linkText + " " + linkHref);
+                }
                 else{
-                    String urlToDownload = urlToParse.substring(urlToParse.indexOf("h"), urlToParse.lastIndexOf("/"));
-                    System.out.println(linkText + " " + urlToDownload + "/" + linkHref);
+                    String urlToDownload = urlToParse.substring(urlToParse.indexOf("h"), urlToParse.lastIndexOf("/")) + "/" + linkHref;
+                    System.out.println(linkText + " " + urlToDownload);
                 }
             }
         }
--- src/com/eazydev/Main.java
+++ src/com/eazydev/Main.java
@@ -6,12 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scaneer = new Scanner(System.in);
-//        String urlToParse = "http://www.unidata.ucar.edu/software/netcdf/examples/files.html";
-        String urlToParse = "https://people.sc.fsu.edu/~jburkardt/data/netcdf/netcdf.html";
-
         System.out.println("Enter url address to parse: ");
-        String url = scaneer.nextLine();
+        String url = scaneer.next();
         HtmlParser parser = new HtmlParser();
-        parser.parser(urlToParse);
+        parser.parser(url);
     }
 }
