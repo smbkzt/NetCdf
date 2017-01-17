d664d375b4c2251be02a58470da0221897ef605d
Bekzat Shayakhmetov <bekzat.95@mail.ru>
2017-01-17 14:45:02 +0700

    fix

--- src/com/eazydev/HtmlParser.java
+++ src/com/eazydev/HtmlParser.java
@@ -7,9 +7,18 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class HtmlParser {
-    private Map<String, String> map = new HashMap<>();
+    private Map<String, String> map;
+    private Reader reader;
+    Scanner scanner;
+
+    public HtmlParser(){
+        map = new HashMap<>();
+        scanner = new Scanner(System.in);
+    }
+
     public void parseIt(String url) {
         String urlToParse;
         int count = 0;
@@ -56,6 +65,10 @@ public class HtmlParser {
                     }
                 }
             }
+            System.out.println("Для открытия файла введите номер: ");
+            int answer = scanner.nextInt();
+            reader = new Reader(map.get(String.valueOf(answer)));
+            reader.ReadWhole();
         }
         catch (IOException e) {
             System.out.println("Недействительная ссылка!");
--- src/com/eazydev/Reader.java
+++ src/com/eazydev/Reader.java
@@ -1,7 +1,54 @@
 package com.eazydev;
+import ucar.nc2.NetcdfFile;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by smbek on 17.01.2017.
  */
 public class Reader {
+    String fileName;
+    NetcdfFile nc = null;
+    NetcdfFile  ncfile = null;
+    Scanner scaneer;
+
+
+    public Reader(String filePath){
+        fileName = filePath;
+        scaneer = new Scanner(System.in);
+    }
+
+    public void ReadWhole(){
+        NetcdfFile nc = null;
+        try {
+
+            nc = NetcdfFile.open(fileName);
+            System.out.println("Краткое опписание файла: " + nc.findGlobalAttribute("title").getStringValue());
+            System.out.println("Хотите открыть файл полностью? д/н ");
+            String answer = scaneer.next();
+
+            if (answer.equals("д")){
+                System.out.println(nc);
+            }
+            else {
+                return;
+            }
+
+        }
+        catch (IOException ioe) {
+            System.out.println("При попытке открыть файл " + fileName + " произошла ошибка: " + ioe);
+
+
+        } finally {
+            if (null != nc) try {
+              nc.close();
+            } catch (IOException ioe) {
+                System.out.println("При попытке закрыть файл " + fileName + " произошла ошибка: " + ioe);
+            }
+        }
+
+    }
+
+
 }
