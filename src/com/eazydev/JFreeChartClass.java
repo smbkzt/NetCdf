3b4339df98e74b7d0704d80db819a044929b551f
Bekzat Shayakhmetov <bekzat.95@mail.ru>
2017-01-18 16:28:27 +0700

    idea is shit

--- src/com/eazydev/JFreeChartClass.java
+++ src/com/eazydev/JFreeChartClass.java
@@ -19,8 +19,8 @@ public class JFreeChartClass extends ApplicationFrame {
         super(applicationTitle);
         org.jfree.chart.JFreeChart xylineChart = ChartFactory.createXYLineChart(
                 chartTitle,
-                "Months",
-                "Score",
+                "Time",
+                "Value",
                 createDataset(array),
                 PlotOrientation.VERTICAL,
                 true, true, false
@@ -30,7 +30,7 @@ public class JFreeChartClass extends ApplicationFrame {
         final XYPlot plot = xylineChart.getXYPlot();
 
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesPaint(0, Color.BLUE);
         renderer.setSeriesStroke(0, new BasicStroke(4.0f));
         plot.setRenderer(renderer);
         setContentPane(chartPanel);
@@ -38,13 +38,11 @@ public class JFreeChartClass extends ApplicationFrame {
 
     private XYDataset createDataset(Array array) {
         final XYSeries dataFromFile = new XYSeries("Precipitation");
-
         for (int i=0; i<array.getSize(); i++){
             dataFromFile.add(i, array.getDouble(i));
         }
         final XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(dataFromFile);
-
         return dataset;
     }
 }
